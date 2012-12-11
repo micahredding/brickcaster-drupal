0f516d7819a909a39e442f64ea672b3380cd7352
Micah Redding <micahtredding@gmail.com>
2012-12-11 13:22:45 -0600

    changes on live site

--- themes/stark copy/page.tpl.php
+++ themes/stark copy/page.tpl.php
@@ -71,15 +71,19 @@
 
     <div id="header-wrapper"><div id="header"><div class="section clearfix">
 
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
+
       <?php if ($site_name || $site_slogan): ?>
         <div id="name-and-slogan">
           <?php if ($site_name): ?>
-              <div id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">
-                  <span><?php print $site_name; ?></span>
-                  <?php print $site_slogan; ?> 
-                </a> 
-              </div>
+              <div id="site-name"><strong>
+                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a> 
+                <?php print $site_slogan; ?> 
+              </strong></div>
           <?php endif; ?> 
         </div> <!-- /#name-and-slogan -->
       <?php endif; ?>
